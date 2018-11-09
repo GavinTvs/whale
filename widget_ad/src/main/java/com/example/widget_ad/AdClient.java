638729f39621018285de266f9ef6a2a6c0729d13
GavinTvs <fuhao@qutoutiao.net>
2018-11-09 19:17:09 +0800

    1. ad todo

---
+++ widget_ad/src/main/java/com/example/widget_ad/AdClient.java
@@ -0,0 +1,10 @@
+package com.example.widget_ad;
+
+/**
+ * @author : gavin
+ * @date 2018/11/9.
+ */
+
+public abstract class AdClient<T> implements IAdClient{
+
+}
---
+++ widget_ad/src/main/java/com/example/widget_ad/IAdClient.java
@@ -0,0 +1,21 @@
+package com.example.widget_ad;
+
+/**
+ * @author : gavin
+ * @date 2018/11/9.
+ */
+
+public interface IAdClient {
+
+    /**
+     * 预拉取Ad
+     */
+    void preFetchAd(int fetchCount);
+
+    /**
+     *
+     */
+
+
+
+}
